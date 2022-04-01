2f22c22426c7a21dd587da3fdb96fc50e8115778
guanwenhao <g16692148509@163.com>
2022-04-01 15:34:58 +0800

    first

---
+++ src/resources/textures.js
@@ -0,0 +1,62 @@
+let billboardTextures = {};
+// 第一块展板图片地址
+billboardTextures.blogTexture = '../src/jsm/blog.png';
+// 第二块展板图片地址
+billboardTextures.musicTexture ='../src/jsm/myui.png';
+// 第三块展板图片地址
+billboardTextures.fundTexture ='../src/jsm/fund.png';
+
+let boxTexture = {};
+// 链接的图标地址
+boxTexture.Github = '../src/jsm/githubLogo.png';
+boxTexture.BiliBili = '../src/jsm/BiliBili.png';
+boxTexture.QQ = '../src/jsm/qq.png';
+boxTexture.mail = '../src/jsm/envelope.png';
+boxTexture.reactIcon = '../src/jsm/react.png';
+boxTexture.allSkills = '../src/jsm/allSkills.png';
+boxTexture.lensFlareMain = '../src/jsm/lensflare0.png';
+boxTexture.skrillex = '../src/jsm/skrillex.png';
+boxTexture.edmText = '../src/jsm/EDM.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+let woodTexture = '../src/jsm/woodTexture.jpg';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/terp-solutions-text.png';  ////第一块板下面的文字
+inputText.activities = '../src/jsm/activities_text.png';  //右下角的文字图片
+inputText.bagholderBetsText = '../src/jsm/bagholderbets-text.png';  //第二块板下面的文字
+inputText.homeSweetHomeText = '../src/jsm/home-sweet-home-text.png'; //第三块板下面的文字
+inputText.staticPortfolio = '../src/jsm/static-portfolio.png'; 
+inputText.pcControl = '../src/jsm/pc-control.png'
+inputText.mobileControl = '../src/jsm/mobile-control.png'
+inputText.link = '../src/jsm/link.png'
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog ='https://guan101.github.io/';  // 第一块板链接地址
+URL.music = 'https://guan101.github.io/wenui/#/';  // 第二块板链接地址
+URL.fund = 'https://juejin.cn/user/4477663475666014/posts';  // 第三块板链接地址
+URL.email = 'https://airhua602@gmail.com';  
+URL.ryanfloyd = 'https://huabyte.com';
+URL.gitHub = 'https://github.com/AirHua-byte';
+URL.BiliBili = 'https://space.bilibili.com/450443708';
+
+// QQ面板跳转
+URL.devTo =
+  'http://wpa.qq.com/msgrd?v=3&uin=3301833942&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:3301833942:41';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
